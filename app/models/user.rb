f9e4574940c593106d63ace4c1e369c7828722eb
wiki <wiki.peng@qq.com>
2017-01-13 14:15:17 +0800

    添加属性与单元测试使email唯一，（但是无法解决数据库中唯一）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # attr_accessor :name, :email
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: {maximum: 255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: { case_sensitive: false }
+            # uniqueness: true
 
   # def initialize(attributes = {})
   #   super
