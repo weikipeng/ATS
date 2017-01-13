c2a67b6523dbb5249bffee0d454bdbbfde7e1212
wiki <wiki.peng@qq.com>
2017-01-13 22:37:13 +0800

    添加注册集成测试

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,4 +4,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: {user: {name: "",
+                                       email: "user@invalid",
+                                       password: "foo",
+                                       password_confirmation: "bar"}}
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
 end
