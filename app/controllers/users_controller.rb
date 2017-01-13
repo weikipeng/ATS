996dc2181e20392f8a4029f1e5bac94b9a55c536
wiki <wiki.peng@qq.com>
2017-01-14 07:23:51 +0800

    注册成功后跳转至用户信息页面，显示欢迎信息

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params) # 不是最终的实现方式
     if @user.save
       # 处理注册成功的情况
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
