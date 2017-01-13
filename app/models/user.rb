26066778936abfd3f1aea24e28b79962b42ef96e
wiki <wiki.peng@qq.com>
2017-01-13 14:52:12 +0800

    添加哈希值gem，添加密码哈希列，保存密码，即使脱库，用户密码依然安全
    改善测试单元使其通过

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,11 @@
 class User < ApplicationRecord
-  # attr_accessor :name, :email
+  before_save { email.downcase! }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},
-            uniqueness: { case_sensitive: false }
-            # uniqueness: true
+            uniqueness: {case_sensitive: false}
 
-  # def initialize(attributes = {})
-  #   super
-  #   @name = attributes[:name]
-  #   @email = attributes[:email]
-  # end
-  #
-  # def formatted_email
-  #   "#{@name} <#{@email}>"
-  # end
-
+  has_secure_password
 end
