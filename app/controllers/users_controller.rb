6ea4ec99684a3023b3b583907396e2332158e16c
wiki <wiki.peng@qq.com>
2017-01-20 10:20:07 +0800

    用户资料编辑成功的测试 注意不更新密码时 允许密码为空，但是密码校验会检测

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       # 处理更新成功的情况
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
