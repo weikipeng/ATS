01a6a51071ed45be9e60bafaf6facbba584051b9
wiki <wiki.peng@qq.com>
2017-01-20 13:00:18 +0800

    记住之前访问的页面，用户登录成功后转向之前的页面 编写单元测试

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,7 @@ class UsersController < ApplicationController
   # 确保用户已登录
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
