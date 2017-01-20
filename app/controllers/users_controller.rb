5edf00fbd6eddf3d844f798fbf798adf226ea018
wiki <wiki.peng@qq.com>
2017-01-20 10:30:02 +0800

    用户编辑和更新时检测用户是否登录 使用测试驱动开发技术完成用户编辑功能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     # debugger
@@ -39,6 +41,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  # 确保用户已登录
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 
 end
