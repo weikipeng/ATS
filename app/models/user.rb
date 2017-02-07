d234bf44cd40f281e4bf0751447c2d4dddb2610f
wiki <wiki.peng@qq.com>
2017-02-07 11:24:28 +0800

    创建激活令牌的回调
    在 User 模型中添加账户激活相关的代码

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
-  before_save { email.downcase! }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -43,4 +44,16 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  private
+
+  # 把电子邮件地址转换成小写
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # 创建并赋值激活令牌和摘要
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
